50901361cc2c10863bb37680a3e6f60a9756abce
Jakob <jakob.leistner@bs-erlangen.de>
2025-01-17 08:23:16 +0100

    endpoint to verify username, password combinations

---
+++ holidayrooms/backend/check_credentials.php
@@ -0,0 +1,32 @@
+<?php
+
+include 'database_connection.php'; 
+$conn = create_db_connection();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Read data from request body
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+
+    $inp_username = $data["username"];
+    $inp_password = $data["password"];
+
+    // Get the password hash for the given user from the DB
+    $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
+    $stmt->bind_param("s", $inp_username);
+    $stmt->execute();
+    $stmt->bind_result($hashed_user_password);
+    $stmt->fetch();
+
+    $stmt->close();
+
+    // Check if the given password corresponds to the password hash in the DB
+    $is_valid = password_verify($inp_password, $hashed_user_password);
+
+    // Return validity state
+    echo json_encode(
+        ["isValid" => $is_valid]
+    );
+}
+
+$conn->close();
