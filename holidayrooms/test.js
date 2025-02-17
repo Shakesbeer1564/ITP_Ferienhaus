eef48dca7983c5e447740fda42963e2f8c960bd9
Jakob <jakob.leistner@bs-erlangen.de>
2025-02-17 11:53:27 +0100

    Added Get Session to HTTP communication POC

--- holidayrooms/test.js
+++ holidayrooms/test.js
@@ -2,10 +2,11 @@ import { HTTPService } from "./http-service.js";
 
 //button events
 document.getElementById('test-click').onclick = loadData;
+document.getElementById('test-session').onclick = getSession;
 
 async function loadData() {
   const data = await HTTPService.getData('data.php');
-  
+
   data.forEach(element => {
     const node = document.createElement('li');
     const text = document.createTextNode(`Name: ${element.name}, Alter: ${element.alter}`);
@@ -13,4 +14,13 @@ async function loadData() {
 
     document.getElementById('testList').appendChild(node);
   });
+}
+
+async function getSession() {
+  const data = await HTTPService.postData("login.php", {
+    "username": "passwordIs:PW",
+    "password": "PW"
+  });
+
+  console.log(data);
 }
