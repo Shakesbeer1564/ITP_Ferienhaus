985142e62592fdc683dccf78ae8a0c75eb1dd45f
Justin-Seidlitz <justin.seidlitz@bs-erlangen.de>
2025-02-18 14:41:48 +0100

    fixing parameter bug for login and register

--- holidayrooms/components/authentification/login/login.js
+++ holidayrooms/components/authentification/login/login.js
@@ -7,14 +7,19 @@ document.getElementById('close_dialog').addEventListener('click', () => {
 
 // sign in button
 document.querySelector('#sign_in').addEventListener('click', async () => {
-  const username = document.getElementById('log_email').value;
+  const email = document.getElementById('log_email').value;
   const password = document.getElementById('password').value;
   
   try {
-    const dataJson = JSON.stringify({
-      username: username,
+    if(!emailValidation(email) || password === ''){
+      console.log('EMAIL OR PASSWORD ARE WRONG');
+      return;
+    }
+
+    const dataJson = {
+      username: email,
       password: password
-    });
+    };
 
     const data = await HTTPService.postData('login.php', dataJson);
 
@@ -25,6 +30,12 @@ document.querySelector('#sign_in').addEventListener('click', async () => {
     console.log('SOMETHING WENT WRING WHILE LOGIN: ', error);
   }
 })
+
+function emailValidation(email){
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  return emailRegex.test(email);
+}
 
 function closeDialog(){
   document.getElementById("dark_background").style.display = 'none';
--- holidayrooms/components/authentification/regionstation/registration.js
+++ holidayrooms/components/authentification/regionstation/registration.js
@@ -9,11 +9,11 @@ document.getElementById('register').addEventListener('click', async () => {
   const tel = document.getElementById('reg_tel').value;
 
   if(username !== '' && password !== '' && emailValidation(email) && telValidation(tel)){
-    const jsonData = JSON.stringify({
+    const jsonData = {
       username: username,
       password: password,
       email: email
-    });
+    };
 
     try {
       const data = await HTTPService.postData('register.php', jsonData);
