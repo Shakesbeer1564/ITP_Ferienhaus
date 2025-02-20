467edd99d4153483ecd71cf78b59fb9a66dd78fe
Jakob <jakob.leistner@bs-erlangen.de>
2025-02-20 08:58:33 +0100

    clean up

--- holidayrooms/backend/endpoints/get_houses.php
+++ holidayrooms/backend/endpoints/get_houses.php
@@ -1,12 +1,9 @@
 <?php
 
-include_once '../functions/database_connection.php';
 include_once '../functions/permission_check.php';
 include_once '../functions/session_check.php';
 include_once '../functions/search.php';
 include_once '../functions/http_communication.php';
-
-$conn = create_db_connection();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
