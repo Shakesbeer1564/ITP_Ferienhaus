bb9f815228213b4179100ea61bcc59272f9328ca
Jakob <jakob.leistner@bs-erlangen.de>
2025-02-25 14:51:53 +0100

    Added display of users booking when user is clicke

--- holidayrooms/components/admin/admin.js
+++ holidayrooms/components/admin/admin.js
@@ -31,14 +31,47 @@ function displayCustomers(customers) {
     });
 }
 
-function onCustomerRowClick(email) {
+function displayBookings(bookings) {
+    const bookingTableBody = document.querySelector('#booking-table tbody');
+    // Clear any existing content
+    bookingTableBody.innerHTML = '';
+
+    console.log(bookings);
+
+    for (let booking of bookings) {
+        const row = document.createElement('tr');
+
+        const startDateCell = document.createElement('td');
+        const endDateCell = document.createElement('td');
+        const priceCell = document.createElement('td');
+
+        startDateCell.textContent = booking.Startdatum;
+        endDateCell.textContent = booking.Enddatum;
+        priceCell.textContent = booking.Preis;
+
+        row.appendChild(startDateCell);
+        row.appendChild(endDateCell);
+        row.appendChild(priceCell);
+
+        bookingTableBody.appendChild(row);
+    }
+}
+
+async function onCustomerRowClick(email) {
+
+    let customerContainer = document.querySelector("#customer-container");
+    let bookingContainer = document.querySelector("#booking-container");
+
+    customerContainer.classList.toggle('hidden');
+    bookingContainer.classList.toggle('hidden');
+
     console.log(email);
 
-    const res = HTTPService.postData('get_users_bookings.php', {
+    const res = await HTTPService.postData('get_users_bookings.php', {
         "userEmail": email
     });
 
-    console.log(res);
+    displayBookings(res);
 }
 
 await loadCustomers();
