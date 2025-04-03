8af2308fccd03e0f16fc4b2833511bb6f3ea575f
Justin-Seidlitz <justin.seidlitz@bs-erlangen.de>
2025-04-03 19:21:50 +0200

    Update Erstellungsskript

---
+++ Erstellungsskript/V2/create_db.bat
@@ -0,0 +1,10 @@
+@echo off
+set MYSQL_USER=root
+set MYSQL_HOST=localhost
+set DATABASE_NAME=Ferienhausverwaltung_Gruppe5
+
+echo Erstelle die Datenbank...
+C:\xampp\mysql\bin\mysql.exe -u %MYSQL_USER% -h %MYSQL_HOST% < db_create.sql
+
+echo Fertig!
+pause
