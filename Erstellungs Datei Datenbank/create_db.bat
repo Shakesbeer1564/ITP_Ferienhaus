a9881de717bc6e53f75533aa867079c71b9982e4
Tom Rödel <166747133+Shakesbeer1564@users.noreply.github.com>
2025-04-03 08:06:49 +0200

    Update create_db.bat

--- Erstellungs Datei Datenbank/create_db.bat
+++ Erstellungs Datei Datenbank/create_db.bat
@@ -1,7 +1,7 @@
 @echo off
 set MYSQL_USER=root
 set MYSQL_HOST=localhost
-set DATABASE_NAME=Ferienhausverwaltung_Gruppe5
+set DATABASE_NAME=Ferienhausverwaltung
 
 echo Erstelle die Datenbank...
 C:\xampp\mysql\bin\mysql.exe -u %MYSQL_USER% -h %MYSQL_HOST% < db_create.sql
